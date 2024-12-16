1697e7a66919d591dbc7dc94cf9d6c351e6117cd
clark21 <clark@dev-engine.net>
2024-12-16 15:11:42 +0800

    adjustments

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include_once (__DIR__ . '/src/Game.php');
 
-$player1 = new Player('Clark'); // uses rock only
+$player1 = new Player('Clark', 'rock'); // uses rock only
 $player2 = new Player('Charles'); // uses rock, paper and scissor
 
 $game = new Game();
